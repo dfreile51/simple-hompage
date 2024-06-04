e9af9cb09eda3e60f4bd0a63889c2e3e45aa94c4
Diego Freile <dfreile51@gmail.com>
2024-06-04 21:12:07 +0200

    Added logic to switch between dark mode and light mode

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const buttonMenu = document.querySelector('header button')
     const buttonClose = document.querySelector('.sidebar button')
     const sidebar = document.querySelector('.sidebar')
+    const inputsCheck = document.querySelectorAll('input[type="checkbox"]')
+    const htmlTag = document.querySelector('html')
+    const LogoImage = document.querySelector('header>img')
+
+    const arrayInputs = [...inputsCheck]
 
     buttonMenu.addEventListener('click', () => {
         sidebar.classList.add('open')
@@ -11,5 +16,37 @@ document.addEventListener('DOMContentLoaded', () => {
 
     buttonClose.addEventListener('click', () => {
         sidebar.classList.remove('open')
+    })
+
+    arrayInputs.forEach(input => {
+        if (localStorage.getItem('theme') === 'dark') {
+            input.checked = false
+            htmlTag.classList.add('dark')
+            LogoImage.src = '/src/img/alarado-icon-homepage-dark.svg'
+            buttonMenu.firstElementChild.src = '/src/img/menu-dark.svg'
+            buttonClose.firstElementChild.src = '/src/img/close-dark.svg'
+        } else {
+            input.checked = true
+            htmlTag.classList.remove('dark')
+            LogoImage.src = '/src/img/alarado-icon-homepage.svg'
+            buttonMenu.firstElementChild.src = '/src/img/menu.svg'
+            buttonClose.firstElementChild.src = '/src/img/close.svg'
+        }
+
+        input.addEventListener('click', e => {
+            if (e.target.checked) {
+                localStorage.setItem('theme', 'light')
+                htmlTag.classList.remove('dark')
+                LogoImage.src = '/src/img/alarado-icon-homepage.svg'
+                buttonMenu.firstElementChild.src = '/src/img/menu.svg'
+                buttonClose.firstElementChild.src = '/src/img/close.svg'
+            } else {
+                localStorage.setItem('theme', 'dark')
+                htmlTag.classList.add('dark')
+                LogoImage.src = '/src/img/alarado-icon-homepage-dark.svg'
+                buttonMenu.firstElementChild.src = '/src/img/menu-dark.svg'
+                buttonClose.firstElementChild.src = '/src/img/close-dark.svg'
+            }
+        })
     })
 })
