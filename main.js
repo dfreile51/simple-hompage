7bef7ac95d576e2950ecfec39b1e9a6c501b5bc0
Diego Freile <dfreile51@gmail.com>
2024-06-04 19:01:44 +0200

    Responsive layout and add sidebar

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 import './src/css/style.css'
+
+document.addEventListener('DOMContentLoaded', () => {
+    const buttonMenu = document.querySelector('header button')
+    const buttonClose = document.querySelector('.sidebar button')
+    const sidebar = document.querySelector('.sidebar')
+
+    buttonMenu.addEventListener('click', () => {
+        sidebar.classList.add('open')
+    })
+
+    buttonClose.addEventListener('click', () => {
+        sidebar.classList.remove('open')
+    })
+})
